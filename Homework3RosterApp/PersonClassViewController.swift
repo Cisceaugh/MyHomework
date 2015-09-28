ec725ba64d1970209a9e8a7a6bd670d3825c182f
Francisco Ragland Jr <franciscoragland@Franciscos-MacBook-Pro.local>
2015-09-28 15:18:30 -0700

    first commit

---
+++ Homework3RosterApp/PersonClassViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PersonClassViewController.swift
+//  Homework3RosterApp
+//
+//  Created by Francisco Ragland Jr on 9/28/15.
+//  Copyright © 2015 Francisco Ragland. All rights reserved.
+//
+
+import UIKit
+
+class PersonClassViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
